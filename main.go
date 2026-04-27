6e37dc351b4d325142156d5c9ef9595b1323b4f1
baharma <bagusaditya1899@gmail.com>
2026-04-27 11:15:51 +0800

    feat: starter CRUD using http

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var PORT = ":8030"
+
+type Emplyee struct {
+	Name string
+	Age  int
+}
+
+var emp = []Emplyee{
+	{Name: "John", Age: 30},
+	{Name: "Jane", Age: 25},
+}
+
+func main() {
+	http.HandleFunc("/", greet)
+	http.HandleFunc("/employees", getEmployees)
+	http.HandleFunc("/employee", createEmployee)
+	fmt.Printf("Server is running on http://localhost%s\n", PORT)
+	http.ListenAndServe(PORT, nil)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	msg := "Hello, World!"
+	fmt.Fprintln(w, msg)
+}
+
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method == http.MethodPost {
+		name := r.FormValue("name")
+		age := r.FormValue("age")
+
+		convertedAge, err := strconv.Atoi(age)
+		if err != nil {
+			http.Error(w, "Invalid age", http.StatusBadRequest)
+			return
+		}
+		newEmployee := Emplyee{Name: name, Age: convertedAge}
+		json.NewEncoder(w).Encode(newEmployee)
+		return
+	}
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+
+func getEmployees(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == http.MethodGet {
+		json.NewEncoder(w).Encode(emp)
+		return
+	}
+
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
